9f38cc9b17216a0bffc6ceca9c0b8527ff9ec59e
marmeladywka <74825525+marmeladywka@users.noreply.github.com>
2021-04-07 14:32:55 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.info__slider').slick({
+		arrows:true,
+		dots:true,
+		adaptiveHeight:true,
+		slidesToShow:4,
+		slidesToScroll:1,
+		speed:1000,
+		easing:'ease',
+		infinite:true,
+		initialSlide:0,
+		autoplay:true,
+		autoplaySpeed:1000,
+		pauseOnFocus:true,
+		pauseOnHover:true,
+		pauseOnDotsHover:true,
+		draggable:true,
+		swipe:true,
+		touchThreshold:10,
+		touchMove:true,
+
+	});
+})
